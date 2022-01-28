86cd7743a779132f27113de4683304326324d954
maykobarbosa <67514858+maykobarbosa@users.noreply.github.com>
2022-01-28 17:31:04 -0300

    começo

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+import { Home } from './src/screens/Home';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if(!fontsLoaded){
+    return (
+      <ActivityIndicator 
+        color={theme.colors.main} 
+        size='large'
+      />
+    )
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import {
+  Container,
+  Header
+} from './styles';
+
+export function Home(){
+  return (
+    <Container>
+      <StatusBar 
+        barStyle="light-content"
+      />
+      <Header>
+
+      </Header>
+      
+    </Container>
+  );
+}
