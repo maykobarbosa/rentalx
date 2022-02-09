56876cb5a73add32e039c3e6005f74ba6d30a49e
maykobarbosa <67514858+maykobarbosa@users.noreply.github.com>
2022-02-08 23:49:45 -0300

    splash

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import { Button, StyleSheet } from 'react-native';
-import Animated, { useSharedValue } from 'react-native-reanimated';
-import { Container } from './styles';
+import React, { useEffect } from 'react';
+import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
+import BrandSvg from '../../assets/brand.svg'
+import LogoSvg from '../../assets/logo.svg'
 
+import {
+  Container
+} from './styles';
 
 export function Splash(){
-  const position = useSharedValue(0)
+  const splashAnimation = useSharedValue(0)
+
+  const brandStyle = useAnimatedStyle(() => {
+    return{ 
+      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value, 
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP
+          )
+        }
+      ]
+    }
+  })
+
+  const logoStyle = useAnimatedStyle(() => {
+    return{ 
+      opacity: interpolate(splashAnimation.value, 
+        [0, 25, 50], 
+        [0, .3, 1]
+      ),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value, 
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP
+          )
+        }
+      ]
+    }
+  })
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(
+      50,
+      {duration: 5000}
+    )
+  },[])
+
   return (
     <Container>
-      <Animated.View style={styles.box} />
-      <Button  title="Mover" onPress={() => {}}  />
+      <Animated.View style={[brandStyle, {position: 'absolute'}]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+      <Animated.View style={[logoStyle, {position: 'absolute'}]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
     </Container>
   );
 }
-
-
-const styles = StyleSheet.create({
-  box: { 
-    width: 100,
-    height: 100,
-    backgroundColor: 'red'
-  }
-})
